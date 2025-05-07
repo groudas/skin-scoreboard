571288098699ca2f4f1437552fc8ff4e45920452
Renato V Machado <groudas@gmail.com>
2025-05-07 00:39:47 -0300

    Hotfix: Fix duplicated files from last merge

--- src/step3_fetch_match_details.js
+++
@@ -1,9 +0,0 @@
-// src/step3_fetch_match_details.js
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-const config = require('./config').step3;
-const { log, ensureDirExists, sleep, readJsonFile, writeJsonFile } = require('./utils');
-// Require openDotaExtraction from modules directory instead of node module as this is now our application customized one.
-const openDotaExtraction = require('./modules/openDotaExtraction');
-
